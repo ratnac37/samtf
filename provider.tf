07be22571f707878158e1bf22122cd7ba6bf3bb8
prasad <prasad@gmail.com>
2021-09-28 22:39:53 +0530

    added

---
+++ provider.tf
@@ -0,0 +1,18 @@
+
+provider "azurerm" {
+  features {}
+  version         = "=2.46.0"
+  subscription_id = "dbd68380-ce63-4235-8632-ddf2852afc17"
+  client_id       = "fe78821d-c02d-449b-a565-fb84a04b63b7"
+  client_secret   = "M-l7Q~~Ra6EN.5K.ZkZyeqO.dStuqCK2Droke"
+  tenant_id       = "5af85c72-b5b8-42e2-b4b8-325ad97c85cd"
+}
+
+terraform {
+  backend "azurerm" {
+    storage_account_name = "remotebackendsa"
+    container_name       = "container2"
+    key                  = "dev.terraform.tfstate" # state file name
+    access_key = "G2JPO5Uiy1srUkHSMAIqbRE2kxBb37w+sW96f7wrOQPwlfqdZ/mxOHyxFDftAJEM2LkKefcBMv+xcz/HxNxT7w=="
+  }
+}
